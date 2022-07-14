257fbb511775232ce30054442930fe08ae142729
Brady Ryun <bradyryun@gmail.com>
2022-07-13 18:47:32 -0600

    Regular, no reocil state.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import {DynamicData} from "./components/dynamic-data";
-import {useRecoilState} from "recoil";
-import {isLoading} from "./state/atom";
 
 function App() {
-  const [loading,] = useRecoilState(isLoading);
   return (
     <div className="App">
-      <DynamicData loading={loading} />
+      <DynamicData />
     </div>
   );
 }
